fac63d9f32c1d91812a42a255fb9165f96b2c690
pcuser@687horton <galen.oneil@nist.gov>
2020-07-20 13:02:55 -0600

    cleanup zaber, use response from zaber to wait instead of sleep

--- instruments/zaber.py
+++ instruments/zaber.py
@@ -1,5 +1,5 @@
 import math
-from time import sleep
+import time
 import struct
 
 from . import serial_instrument
@@ -11,18 +11,37 @@ from . import serial_instrument
 # Class to control the Zaber motor via serial port. This is usually used
 # as a heat switch.
 #
+# protocol reference:
+# https://www.zaber.com/protocol-manual?protocol=Binary#topic_action_055_echo_data
+#
 # by Doug Bennett
 # Based of similar class for Matlab by Dan Schmidt
 # modified by Frank Schima
 ##########################################################################
 
+ZABER_ERRORS = {
+    1  : "cannot home",
+    2  : "device number invalid",
+    14 : "voltage low",
+    15 : "voltage high",
+    17 : "stored position invalid",
+    18 : "stored postion invalid",
+    20 : "absolute position invalid",
+    21 : "relative position invalid",
+    22 : "velocity invalid",
+    36 : "peripheral id invalid",
+    37 : "resolution invalid",
+    38 : "run current invalid",
+    39 : "hold current invalid",
+    64 : "requested command number is invalid in this firmware version"
+} # this dict is incomplete, but basically it just means the command failed and the keyis the command number
 
 class Zaber(serial_instrument.SerialInstrument):
 
 ######################################################################################
     # Zaber class
 
-    def __init__(self, port='zaber', baud=9600, shared=True, unit=0):
+    def __init__(self, port='zaber', baud=9600, shared=True, unit=1):
         '''Zaber motor - parameters:
         port - a logical portname defined in namedserialrc
         baud -  9600 for zaber
@@ -31,7 +50,7 @@ class Zaber(serial_instrument.SerialInstrument):
         unit - Unit number. 0 for all.
         '''
 
-        super(Zaber, self).__init__(port, baud, shared,readtimeout=1,min_time_between_writes=0.0)
+        super(Zaber, self).__init__(port, baud, shared, readtimeout=1, min_time_between_writes=0.5)
 
         self.manufacturer = 'Zaber'
         self.model_number = 'T-NM17A200'
@@ -52,77 +71,71 @@ class Zaber(serial_instrument.SerialInstrument):
         self.SlowRevs = 2  #how many revs to open slowly at high torque
         self.ClosingSpeed = 1000
         self.ClosingCurrent = 10 #less than full or not
-
+        self.debug = False # set to true to enable debug prints
+    
 ########################################### Private Methods #################################################
 
+    def _debug(self, s):
+        if self.debug: print(s)
+
     def __getBytes(self, value):
-
-        #convert Data to 4 bytes for serial writing
-
-        if value >= 0:
-            #if positive convert normally
-            bytevals = [0, 0, 0, 0]
-            bytevals[3]= int(math.floor(value/(256*256*256)))
-            value = value - (256*256*256)* bytevals[3]
-            bytevals[2] = int(math.floor(value/(256*256)))
-            value = value - (256*256)* bytevals[2]
-            bytevals[1] = int(math.floor(value/(256)))
-            bytevals[0] = int(value - 256* bytevals[1])
-        else:
-            #two's complement for negative
-            value = -1*value
-            value = int(value)
-            bytevals = [0, 0, 0, 0]
-            bytevals[3]= math.floor(value/(256*256*256))
-            value = value - (256*256*256)* bytevals[3]
-            bytevals[2] = math.floor(value/(256*256))
-            value = value - (256*256)* bytevals[2]
-            bytevals[1] = math.floor(value/(256))
-            bytevals[0] = value - 256* bytevals[1]
-
-            bytevals[3] = int(255 - bytevals[3])
-            bytevals[2] = int(255 - bytevals[2])
-            bytevals[1] = int(255 - bytevals[1])
-            #bytevals[0] = int(255 - bytevals[0] + 1)
-            bytevals[0] = int(255 - bytevals[0])
-
-        return bytevals
-
-    def __writeBytes(self, bytestowrite):
-
-        result = self.serial.write()
-        if result != len(bytestowrite):
-            print(('Error in serial write to Zaber, result, len(bytestowrite)', result, len(bytestowrite)))
-            raise Exception()
-        return result
-
+        assert isinstance(value, int)
+        # convert Data to 4 bytes for serial writing, following algorithm
+        # in Appendix B of zaber binary protocol manual
+        if value < 0: # handle negatie values by calculating the twos complement
+            value += 256**4
+        b = [0, 0, 0, 0]
+        b[3] = value // 256**3
+        value -= b[3]*256**3
+        b[2] = value // 256**2
+        value -= b[2]*256**2
+        b[1] = value // 256
+        value -= b[1]*256
+        b[0] = value
+        return b
 
     def __sendCommand(self, value, command):
+        self._debug(f"value={value} command={command}")
+        value_bytes = self.__getBytes(value)
+        b = [self.unit, command] + value_bytes        
+        nwritten = self.serial.write(bytes(b))
+        assert nwritten == len(b), f"tried to write {len(b)} bytes, actually wrote {nwritten}"
+        self._debug(f"bytes being sent{b}")        
 
-        bytevals = self.__getBytes(value)
-        bytevals.insert(0, command)
-        bytevals.insert(0, self.unit)
+    def __sendCommandParseReply(self, value, command):
+        self.__sendCommand(value, command)
+        reply = self.serial.read(6) # will block for timeout
+        return self.__parseReplyToInt(command, reply, value)
 
-        self.write(bytes(bytevals))
-        return self.serial.read(6)
+    def __parseReplyToInt(self, command, reply, command_value):
+        self._debug(f"reply={reply}")
+        if len(reply) == 0:
+            raise Exception(f"command {command} got empty reply")
+        elif len(reply) > 6:
+            raise Exception(f"reply too long, length={len(reply)} reply={reply}")
+        elif len(reply) != 6: 
+            raise Exception(f"all reply should be length 6, got length {len(reply)}, reply={reply}")
+        b = struct.unpack("BBBBBB", reply)
+        self._debug(f"reply_bytes={b}")
+        assert b[0] == self.unit
+        v = b[2] + b[3]*256 + b[4]*256**2 + b[5]*256**3
+        if b[5] > 127: # handle twos complement
+            v -= 256**4
+        if b[1] == 255:
+            raise Exception(self.__errorString(v, command, command_value))
+        else:
+            assert b[1] == command, f"2nd reply byte was {b[1]}, should equal the command {command}"
+        return v
+    
+    def __errorString(self, v, command, command_value):
+        error_string = f"zaber command {command} with data {command_value} failed with code {v}: " + ZABER_ERRORS.get(v, f"unknown code")
+        return error_string
 
 ########################################### Public Methods #################################################
 
     def getFirmwareVersion(self):
-
-        Command = 51
-
-        value = self.__sendCommand(value=0, command=Command)
-
-        version1, version2, version3, version4, version5, version6 = struct.unpack("BBBBBB", value)
-
-        # Convert data to integer value
-        version = 256**3 * version6 + 256**2 * version5 + 256 * version4 + version3
-        if version6 > 127:
-            version = version - 256^4
-
-        version_float = version / 100.0
-
+        reply_int = self.__sendCommandParseReply(value=0, command=51)
+        version_float = reply_int / 100.0
         return version_float
 
     def close(self):
@@ -130,59 +143,39 @@ class Zaber(serial_instrument.SerialInstrument):
 
     def Stop(self):
         #stop motor
-
-        Command = 23
-
-        self.__sendCommand(value=0, command=Command)
-
+        self.__sendCommandParseReply(value=0, command=23)
 
     def MoveConstantVelocity(self, velocity=500):
         #constant velocity move , sign indicates direction
         #keeps moving until another command is issued
-
-        Command = 22
-
-        self.__sendCommand(value=velocity, command=Command)
+        self.__sendCommandParseReply(value=velocity, command=22)
 
 
     def SetCurrentPosition(self, position=200000):
         #set current position
-
-        Command = 45
-
-        self.__sendCommand(value=position, command=Command)
+        self.__sendCommandParseReply(value=position, command=45)
 
 
     def SetTargetVelocity(self, velocity=50):
         #set target velocity
-
-        Command = 42
-
-        self.__sendCommand(value=velocity, command=Command)
+        self.__sendCommandParseReply(value=velocity, command=42)
 
 
     def SetHoldCurrent(self, current=0):
         #set running current
         #set running current Range 0, 10-127   0: no current, 10 max , 127 min
-
-        Command = 39
-
-        self.__sendCommand(value=current, command=Command)
+        self.__sendCommandParseReply(value=current, command=39)
 
     def SetRunningCurrent(self, current=10):
         #set running current
         #set running current Range 0, 10-127   0: no current, 10 max , 127 min
+        self.__sendCommandParseReply(value=current, command=38)
 
-        Command = 38
-
-        self.__sendCommand(value=current, command=Command)
-
-    def MoveRelative(self, steps=0):
-        #move steps # of microsteps
-
-        Command = 21
-
-        self.__sendCommand(value=steps, command=Command)
+    # dont use this, because it doesnt return a reply until it finishes, so you need logic to
+    # to wait for the reply
+    # def MoveRelative(self, steps=0):
+    #     #move steps # of microsteps
+    #     self.__sendCommandParseReply(value=steps, command=21)
 
     def OpenHeatSwitch(self, OpeningRevs = None, SlowRevs = None):
 
@@ -192,34 +185,86 @@ class Zaber(serial_instrument.SerialInstrument):
         if SlowRevs is None:
             SlowRevs = self.SlowRevs
 
-        #print 'high torque low speed'
+        # high torque low speed
         self.SetRunningCurrent(self.OpeningCurrent)
-        sleep(1)
         self.SetTargetVelocity(self.OpeningSpeed)
-        sleep(1)
-        self.SetCurrentPosition(200000)   #set to middle of range
-        sleep(1)
-        self.MoveRelative(int(SlowRevs*self.MicroStepsPerRev)) # open 2 revolutions
-        sleep(SlowRevs/(self.OpeningSpeed*self.SpeedConvert)*1.2) #wait for motor to finish 1.2 to be certain
+        self.SetCurrentPosition(1000000) # makes negative relative moves work
+        self.MoveRelativeThenWait(int(SlowRevs*self.MicroStepsPerRev)) # open 2 revolutions
+        # time.sleep(SlowRevs/(self.OpeningSpeed*self.SpeedConvert)*1.2) # wait for motion to complete
 
-        #print 'drop torque , up speed and finish'
+
+        # drop torque , up speed and finish
         self.SetRunningCurrent(self.ClosingCurrent)
-        sleep(1)
         self.SetTargetVelocity(self.ClosingSpeed)
-        sleep(1)
-        self.MoveRelative(int((OpeningRevs-SlowRevs+.5)*self.MicroStepsPerRev)) # open 8.5 more revs
-        sleep((OpeningRevs-SlowRevs+.5)/(self.ClosingSpeed*self.SpeedConvert)*1.3) #wait for motor to finish 1.3 to be certain
+        self.SetCurrentPosition(1000000) # makes negative relative moves work
+        self.MoveRelativeThenWait(int((OpeningRevs-SlowRevs)*self.MicroStepsPerRev)) # open rest of revs
+        # time.sleep((OpeningRevs-SlowRevs)/(self.ClosingSpeed*self.SpeedConvert)*1.2) # wait for motion to complete
 
     def CloseHeatSwitch(self, ClosingRevs = None):
 
         if ClosingRevs is None:
             ClosingRevs = self.OpeningRevs + 2 # +2 to ensure it closes
 
-        #print 'closing heat switch'
         self.SetRunningCurrent(self.ClosingCurrent)
-        sleep(1)
         self.SetTargetVelocity(self.ClosingSpeed)
-        self.SetCurrentPosition(200000)   #set to middle of range
-        #move relative - OpeningRevs + 2revolutions (negative to give clockwise rotation)
-        self.MoveRelative(int(-(ClosingRevs)*self.MicroStepsPerRev))
-        sleep(ClosingRevs/(self.ClosingSpeed*self.SpeedConvert)*1.3)
+        self.SetCurrentPosition(1000000) # makes negative relative moves work
+        self.MoveRelativeThenWait(int(-ClosingRevs*self.MicroStepsPerRev))
+        # time.sleep(ClosingRevs/(self.ClosingSpeed*self.SpeedConvert)*1.2)
+
+    def getPosition(self):
+        value = self.__sendCommandParseReply(value=0, command=60)
+
+        return value
+
+    def echo(self, v):
+        """
+        send value v to the zaber, the zaber should return the same value
+        useful for testing
+        """
+        assert isinstance(v, int)
+        return self.__sendCommandParseReply(value=v, command=55)
+
+    def getCurrentPosition(self):
+        return self.__sendCommandParseReply(value=0, command=60)
+
+    def getSerialNumber(self):
+        return self.__sendCommandParseReply(value=0, command=63)
+
+    def getStatus(self):
+        code = self.__sendCommandParseReply(value=0, command=54)
+        interp_dict = {
+            0  : "idle",
+            21 : "motion", # seems to be returned during motion, even though manual says it should be 99
+            65 : "parked",
+            90 : "diabled",
+            93 : "inactive",
+            99 : "motion"
+        }
+        if code not in interp_dict:
+            raise Exception(f"status code = {code} invalid, not in interp_dict = {interp_dict}")
+        return interp_dict[code]
+
+    def getPowerSupplyVoltage(self):
+        return self.__sendCommandParseReply(value=0, command=52)
+
+    def getEncoderPosition(self):
+        return self.__sendCommandParseReply(value=0, command=89)
+
+    def waitForRelativeMoveToComplete(self):
+        """ this function should work, but it always causes an error of some sort, 
+        don't expect it to work"""
+        while True:
+            reply = self.serial.read(6)
+            if len(reply) == 0:
+                continue
+                # move is not complete, because it hasn't responded
+            elif len(reply) == 6:
+                b = struct.unpack("BBBBBB", reply)
+                assert b[1] == 21, f"expected respond to move relative (21), got {b[1]}"    
+                break
+            else:
+                raise Exception(f"reply should be length 0 or 6, got {length(reply)}, reply={reply}")
+
+    def MoveRelativeThenWait(self, steps):
+        self.__sendCommand(value=steps, command=21)
+        self.waitForRelativeMoveToComplete()
