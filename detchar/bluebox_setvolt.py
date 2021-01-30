7b212059a18832eed1f389142b41a5cb028e0178
Hannes <hubmayr@nist.gov>
2021-01-30 11:08:30 -0700

    bluebox_setvolt: solve 1st command not executed after bluebox power cycle

--- detchar/bluebox_setvolt.py
+++ detchar/bluebox_setvolt.py
@@ -7,3 +7,4 @@ import sys
 vs = bluebox.BlueBox(port='vbox', version='mrk2')
 v=float(sys.argv[1])
 vs.setvolt(v)
+vs.setvolt(v) # command repeated because after power cycle 1st command doesn't take
