8f3fac12a0eab3013469f380a65de3062806af36
Hubmayr <hubmayr@687jhmbp.campus.nist.gov>
2021-07-06 22:05:22 -0600

    docstring update to bluebox_setvolt.py

--- detchar/bluebox_setvolt.py
+++ detchar/bluebox_setvolt.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
-'''test_bluebox.py '''
+'''bluebox_setvolt.py '''
 
-from instruments import bluebox 
-import sys 
+from instruments import bluebox
+import sys
 
 vs = bluebox.BlueBox(port='vbox', version='mrk2')
 v=float(sys.argv[1])
