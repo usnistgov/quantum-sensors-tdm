86ab37272e2faedbcda2b4ce07db04b0257488c4
Hubmayr <hubmayr@687jhmbp.campus.nist.gov>
2021-07-14 20:59:59 -0600

    add plot() method to PolCalSteppedSweepData class within polcal.py

--- detchar/polcal.py
+++ detchar/polcal.py
@@ -1,7 +1,7 @@
 ''' polcal.py
 
-    Polarization calibration software using aerotech XY stage, velmex stepper motor controlled wire grid, 
-    function generator biased IR source, and NIST `previous generation (TDM) electronics' 
+    Polarization calibration software using aerotech XY stage, velmex stepper motor controlled wire grid,
+    function generator biased IR source, and NIST `previous generation (TDM) electronics'
 
     JH 6/2021, based on Jay's software for legacy electronics.
 '''
@@ -33,8 +33,8 @@ from tools import SoftwareLockinAcquire
 # 2) step through grid positions, for each grid position get lock-in signal from each bolometer on the column.
 #
 # Data I would like returned:
-# - locked in signal for each detector for each grid angular position 
-#     - locked in signal data format: I,Q and rms of each?  
+# - locked in signal for each detector for each grid angular position
+#     - locked in signal data format: I,Q and rms of each?
 #     - grid position for this measurement
 # - meta-data:
 #         - row select -> state map
@@ -52,8 +52,8 @@ class PolCalSteppedSweepData():
     row_order: List[int]
     #bayname: str
     #db_cardname: str
-    column_number: int    
-    source_amp_volt: float 
+    column_number: int
+    source_amp_volt: float
     source_offset_volt: float
     source_frequency_hz: float
     #nominal_temp_k: float
@@ -74,41 +74,66 @@ class PolCalSteppedSweepData():
         with open(filename, "r") as f:
             return cls.from_json(f.read())
 
-    def plot(self):
-        plt.figure()
-        plt.plot(self.angle_deg, self.iq_values)
-        plt.xlabel("Angle (deg)")
-        plt.ylabel("Response (arb)")
-        plt.title(f"bay {self.bayname}, db_card {self.db_cardname}, nominal_temp_mk {self.nominal_temp_k*1000}")
+    def plot(self, rows_per_figure=None):
+        ''' rows_per_figure is a list of lists to group detector responses
+            to be plotted together.  If None will plot in groups of 8.
+        '''
+        if rows_per_figure is not None:
+            pass
+        else:
+            n_angles,n_rows,n_iq = np.shape(self.iq_v_angle)
+            n_groups = n_rows//num_in_group + 1
+            rows_per_figure=[]
+            for jj in range(n_groups):
+                tmp_list = []
+                for kk in range(8):
+                    row_index = jj*num_in_group+kk
+                    if row_index>=n_rows: break
+                    tmp_list.append(row_index)
+                rows_per_figure.append(tmp_list)
+        print rows_per_figure
+        for ii,row_list in enumerate(rows_per_figure):
+            fig,ax = plt.subplots(3,num=ii)
+            for row in row_list:
+                ax[0].plot(self.angle_deg,self.iq_v_angle[:,row,0],'o-',label=jj)
+                ax[1].plot(self.angle_deg,self.iq_v_angle[:,row,1],'o-',label=jj)
+                ax[2].plot(self.angle_deg,np.sqrt(self.iq_v_angle[:,row,0]**2+self.iq_v_angle[:,ii,1]**2),'o-',label=jj)
+            ax[0].y_label('I (DAC)')
+            ax[1].y_label('Q (DAC)')
+            ax[2].y_label('Amplitude (DAC)')
+            ax[2].x_label('Angle (deg)')
+            ax[1].legend()
+            fig.title('Column %d, Group %d'%(self.column_number,ii))
+        plt.show()
 
 class PolcalSteppedSweep():
     ''' Aquire polcal at stepped, fixed angles '''
-    def __init__(self, angle_list_deg, 
-                 source_amp_volt=3.0, source_offset_volt=1.5, source_frequency_hz=5.0, 
+    def __init__(self, angle_list_deg,
+                 source_amp_volt=3.0, source_offset_volt=1.5, source_frequency_hz=5.0,
                  num_lockin_periods = 10,
                  row_order=None):
-        
+
         # hardware and class initialization
-        self.sla = SoftwareLockinAcquire(easy_client=None, signal_column_index=0,reference_column_index=1, 
+        self.sla = SoftwareLockinAcquire(easy_client=None, signal_column_index=0,reference_column_index=1,
                                          signal_feedback_or_error='error',num_pts_per_period=None)
-        self.source = Agilent33220A() 
+        self.source = Agilent33220A()
         self.adr_gui_control = AdrGuiControl() #self._handle_adr_gui_control_arg(adr_gui_control)
         self.init_source(source_amp_volt, source_offset_volt, source_frequency_hz)
         self.grid_motor = Velmex(doInit=True)
-        
+
         # input parameters
-        self.angle_deg, self._angle_100th_deg = self._handle_angle_arg(angle_list_deg)  
+        self.angle_deg, self._angle_100th_deg = self._handle_angle_arg(angle_list_deg)
         self.num_angles = len(self.angle_deg)
         self.source_amp_v = source_amp_volt
         self.source_offset_v = source_offset_volt
-        self.source_freq_hz = source_frequency_hz 
+        self.source_freq_hz = source_frequency_hz
         self.num_lockin_periods = 10
         self.row_order = self._handle_row_to_state(row_order)
         self.waittime_s = 10.0 # time to wait between setting grid angle and acquiring data
 
     def _handle_row_to_state(self,row_order):
         if row_order is not None:
-            return row_order 
+            return row_order
         return list(range(self.sla.ec.nrow))
 
     def _handle_angle_arg(self,angle):
@@ -128,20 +153,20 @@ class PolcalSteppedSweep():
         self.source.SetOutput(outputstate='on')
 
     def get_point(self,window=False):
-        return self.sla.getData(num_periods=self.num_lockin_periods, window=window,debug=False) 
+        return self.sla.getData(num_periods=self.num_lockin_periods, window=window,debug=False)
 
     def get_polcal(self, extra_info = {}):
         pre_time = time.time()
         pre_temp_k = self.adr_gui_control.get_temp_k()
-        iq_v_angle = np.empty((self.num_angles,self.sla.ec.nrow,2))
         angle_100th_deg_diff = np.diff(np.array(self._angle_100th_deg))
         angle_100th_deg_diff=np.insert(angle_100th_deg_diff,0,0)
+        iq_v_angle = np.empty((self.num_angles,self.sla.ec.nrow,2))
         for ii, angle in enumerate(angle_100th_deg_diff):
             print('Moving to angle = %.1f deg'%(self.angle_deg[ii]))
             self.grid_motor.move_relative(phi_100th_deg=angle,wait=False,verbose=False)
             time.sleep(self.waittime_s)
             iq_arr = self.get_point() # nrow x 2 array
-            iq_v_angle[ii,:,:] = iq_arr 
+            iq_v_angle[ii,:,:] = iq_arr
         post_temp_k = self.adr_gui_control.get_temp_k()
         post_time = time.time()
 
@@ -160,17 +185,16 @@ class PolcalSteppedSweep():
                                       pre_time_epoch_s=pre_time,
                                       post_time_epoch_s=post_time,
                                       extra_info=extra_info)
-    
+
 class PolCalSteppedBeamMap():
-    ''' Aquire PolcalSteppedSweep for x,y positions ''' 
+    ''' Aquire PolcalSteppedSweep for x,y positions '''
     def __init__(self):
         print('To be written')
 
 if __name__ == "__main__":
-    pcss = PolcalSteppedSweep(angle_list_deg=[0,45,90], 
-                       source_amp_volt=.1, source_offset_volt=0.5, source_frequency_hz=5.0, 
+    pcss = PolcalSteppedSweep(angle_list_deg=[0,45,90],
+                       source_amp_volt=.1, source_offset_volt=0.5, source_frequency_hz=5.0,
                        num_lockin_periods = 10,
                        row_order=None)
     pc_data = pcss.get_polcal()
     pc_data.to_file('test_polcal_data',overwrite=True)
-    
