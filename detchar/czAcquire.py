c61ae39758a04abdd7edaca901d38cf4c61b798a
Hannes <hubmayr@nist.gov>
2023-05-05 18:29:40 -0600

    czAcquire.py skeleton tested in the superconducting branch.  Need to add custom return data structure.

--- detchar/czAcquire.py
+++ detchar/czAcquire.py
@@ -1,15 +1,17 @@
 ''' czAcquire.py
 
-    Acquire data for complex impedance measurements
-    Work in progress!
+    @authors GCJ and JH 5/2023, based polcal.py.
 
-    @author GCJ 1/2022, based Hannes's polcal.py, based on Jay's software for legacy electronics.
+    Typical hardware setup: 
+    1) Agilent33220A output through resistor box (10K) into detector bias line
+    2) Agilent33220A sync into DFB card CH2 input.  This corresponds to reference_column_index=1.
+    3) Detector column signal corresponds to DFB card CH1 input/output.
 '''
 
 from nasa_client import EasyClient
 from instruments import BlueBox, Agilent33220A 
 from adr_gui.adr_gui_control import AdrGuiControl
-from iv_data import ComplexImpedanceSweepData
+#from iv_data import ComplexImpedanceSweepData
 
 import time
 import numpy as np
@@ -54,22 +56,23 @@ def test_for_signal(N_lockins=5):
 
     plt.show()
     
-        
-    
-
-class ComplexImpedanceSweep():
-    ''' Acquire  '''
+class SineSweep():
+    ''' Sweep constant amplitude sine wave as a function of frequency.  
+        Used for example in a single detector bias position for a complex impedance or complex responsivity 
+        measurement.
+    '''
     def __init__(self,
-                 source_amp_volt=3.0, source_offset_volt=1.5, source_frequency_hz=np.logspace(0,5,50),
+                 source_amp_volt=0.04, source_offset_volt=0, source_frequency_hz=np.logspace(1,3,50),
                  num_lockin_periods = 10,
                  row_order=None):
+
+        self.source = Agilent33220A()
+        self.adr_gui_control = AdrGuiControl() #self._handle_adr_gui_control_arg(adr_gui_control)
+        self.init_source(source_amp_volt, source_offset_volt, source_frequency_hz[0])
 
         # hardware and class initialization
         self.sla = SoftwareLockinAcquire(easy_client=None, signal_column_index=0,reference_column_index=1,
                                          signal_feedback_or_error='feedback',num_pts_per_period=None)
-        self.source = Agilent33220A()
-        self.adr_gui_control = AdrGuiControl() #self._handle_adr_gui_control_arg(adr_gui_control)
-        self.init_source(source_amp_volt, source_offset_volt, source_frequency_hz)
 
         # input parameters
         self.source_amp_v = source_amp_volt
@@ -79,7 +82,8 @@ class ComplexImpedanceSweep():
         self.waittime_s = 0.1 # time to wait between setting new frequency and acquiring data
         self.num_freq = len(self.source_freq_hz)
         self.row_order = self._handle_row_to_state(row_order)
-
+        self.freq_hz_measured = None  
+        self.iq_v_freq = None
 
     def _handle_row_to_state(self,row_order):
         if row_order is not None:
@@ -88,7 +92,6 @@ class ComplexImpedanceSweep():
 
     def init_source(self, amp, offset, frequency):
         self.source.SetFunction(function = 'sine')
-        #self.source.SetFunction(function = 'square')
         self.source.SetLoad('INF')
         self.source.SetFrequency(frequency)
         self.source.SetAmplitude(amp)
@@ -98,17 +101,17 @@ class ComplexImpedanceSweep():
     def get_point(self,window=False):
         return self.sla.getData(num_periods=self.num_lockin_periods, window=window,debug=False)
 
-    def get_complexImpedance(self, extra_info = {}, move_to_zero_at_end = True, turn_off_source_on_end = True):
+    def take(self, extra_info = {}, move_to_zero_at_end = True, turn_off_source_on_end = True):
         pre_time = time.time()
         pre_temp_k = self.adr_gui_control.get_temp_k()
 
-        iq_v_freq = np.empty((self.num_freqs,self.sla.ec.nrow,2))
-        measured_freqs = []
+        freq_hz_measured = []
+        iq_v_freq = np.empty((self.num_freq,self.sla.ec.nrow,2))
+        
         for ii, freq in enumerate(self.source_freq_hz):
             print('Setting function generator to frequency = %.2f Hz'%freq)
             self.source.SetFrequency(freq)
-            m_freq = self.source.GetFrequency()
-            measured_freqs.append(m_freq)
+            freq_hz_measured.append(self.source.GetFrequency())
             iq_arr = self.get_point() # nrow x 2 array
             iq_v_freq[ii,:,:] = iq_arr
         post_temp_k = self.adr_gui_control.get_temp_k()
@@ -117,33 +120,44 @@ class ComplexImpedanceSweep():
         print('Acquisition complete.')
         if turn_off_source_on_end: self.source.SetOutput('off')
 
-        return ComplexImpedanceSweepData(iq_v_freq = iq_v_freq,
-                                      row_order=self.row_order,
-                                      #bayname=self.bayname,
-                                      #db_cardname=self.db_cardname,
-                                      column_number=0,
-                                      source_amp_volt=self.source_amp_v,
-                                      source_offset_volt=self.source_offset_v,
-                                      source_frequency_hz=self.source_freq_hz,
-                                      pre_temp_k=pre_temp_k,
-                                      post_temp_k=post_temp_k,
-                                      pre_time_epoch_s=pre_time,
-                                      post_time_epoch_s=post_time,
-                                      extra_info=extra_info)
+        self.freq_hz_measured = freq_hz_measured 
+        self.iq_v_freq = iq_v_freq 
 
+        # return ComplexImpedanceSweepData(iq_v_freq = iq_v_freq,
+        #                               row_order=self.row_order,
+        #                               #bayname=self.bayname,
+        #                               #db_cardname=self.db_cardname,
+        #                               column_number=0,
+        #                               source_amp_volt=self.source_amp_v,
+        #                               source_offset_volt=self.source_offset_v,
+        #                               source_frequency_hz=self.source_freq_hz,
+        #                               pre_temp_k=pre_temp_k,
+        #                               post_temp_k=post_temp_k,
+        #                               pre_time_epoch_s=pre_time,
+        #                               post_time_epoch_s=post_time,
+        #                               extra_info=extra_info)
+
+    def plot(self,fignum=1):
+        fig, ax = plt.subplots(nrows=2,ncols=2,sharex=False,figsize=(12,8),num=fignum)
+        for ii in range(self.sla.ec.nrow):
+            ax[0][0].plot(self.freq_hz_measured,self.iq_v_freq[:,ii,0],'o-')
+            ax[0][1].plot(self.freq_hz_measured,self.iq_v_freq[:,ii,1],'o-')
+            ax[1][0].plot(self.freq_hz_measured,self.iq_v_freq[:,ii,0]**2+self.iq_v_freq[:,ii,1]**2,'o-')
+            ax[1][1].plot(self.freq_hz_measured,np.arctan(self.iq_v_freq[:,ii,1]/self.iq_v_freq[:,ii,0]),'o-')
+
+        # axes labels
+        ax[0][0].set_ylabel('I')
+        ax[0][1].set_ylabel('Q')
+        ax[1][0].set_ylabel('I^2+Q^2')
+        ax[1][1].set_ylabel('Phase')
+        ax[1][0].set_xlabel('Freq (Hz)')
+        ax[1][1].set_xlabel('Freq (Hz)')    
+
+        ax[1][1].legend(list(range(self.sla.ec.nrow)))
 
 if __name__ == "__main__":
-    freqs = np.logspace(0,5,50) # Hz
-    power_line = 60. # Hz
-    delta = 3 # Hz trim freqs within +-delta of power_line
-    source_frequency_hz = list(freqs[(freqs<power_line-delta)]) + list(freqs[(freqs>power_line+delta)])
-    czss = ComplexImpedanceSweep(source_frequency_hz=source_frequency_hz,num_lockin_periods = 10)
-
-
-
-    cz_data = czss.get_complexImpedance()
-    cz_data.to_file('test_cz_data',overwrite=True)
-    cz_data.plot()
-
-    #test_for_signal()
+    ss = SineSweep()
+    ss.take()
+    ss.plot()
+    plt.show()
 
