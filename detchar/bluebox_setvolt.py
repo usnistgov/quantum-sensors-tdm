408547b696862303461ed04ccf724f8e8c221384
Hannes <hubmayr@nist.gov>
2024-05-07 14:05:22 -0600

    switch bluebox_setvolt to ADU instead of physical units

--- detchar/bluebox_setvolt.py
+++ detchar/bluebox_setvolt.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python
 '''bluebox_setvolt.py '''
 
-from instruments import bluebox
+from instruments import BlueBox
 import sys
 
-vs = bluebox.BlueBox(port='vbox', version='mrk2')
-v=float(sys.argv[1])
-vs.setvolt(v)
-vs.setvolt(v) # command repeated because after power cycle 1st command doesn't take
-print('Setting bluebox voltage to %.3f V'%v)
+vs = BlueBox(port='vbox', version='mrk2',baud=9600, address=0, channel=0, shared=True)
+dac=int(sys.argv[1])
+vs.setVoltDACUnits(dac)
+#vs.setvolt(v) # command repeated because after power cycle 1st command doesn't take
+print('Setting bluebox voltage to %.4f V'%(dac/(2**16-1)*6.5535))
