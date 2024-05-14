8d71d02bdc7cc70c3763078f536971b61b5fca96
Joe Fowler <drjoefowler@gmail.com>
2024-05-14 12:34:13 -0600

    Remove redundant info from `setup.py`

--- setup.py
+++ setup.py
@@ -5,21 +5,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    author="GCO",
-    author_email='galen.oneil@nist.gov',
-    python_requires='>=3.5',
-    description="Software to help run a NIST TDM system with Python 3.",
-    install_requires=["matplotlib", "numpy", "PyQt5", "pySerial", "LabJackPython", "lxml",
-                      "argparse", "zmq", "scipy", "pytest", "pytest-qt", "ipython", "pyvisa", "progress",
-                      "dataclasses_json", "lmfit"],
     license="MIT license",
     include_package_data=True,
     keywords='tdm, tes',
-    name='nistqsptdm',
     packages=find_packages(),
     test_suite='tests',
     url='',
-    version='0.1.0',
     zip_safe=False,
     package_data={'': ['*.png', '*.ui']},
     entry_points={
