f6ca3bc4358aa33b1c92c1ac6d3600e3c53f9bb9
Christopher Rooney <christopher.rooney@nist.gov>
2024-08-26 11:06:38 -0600

    Move script to main method, no longer import *

--- detchar/ivCurveAcquire.py
+++ detchar/ivCurveAcquire.py
@@ -15,7 +15,7 @@ v2: 5/2023 updated to use the tower card detector bias
 '''
 
 import yaml, sys, os
-from iv_utils import *
+import iv_utils as iv
 import matplotlib.pyplot as plt
 
 def plot_iv_groups(iv_temp_sweep_inst,num_in_group=8):
@@ -79,72 +79,76 @@ def create_filename():
     write_filename = filename+'.json'
     return write_filename
 
-n_args = len(sys.argv)
-config_filename = str(sys.argv[1])
-if n_args > 2:
-    desc = str(sys.argv[2])
-else:
-    desc=''
+def main():
+    n_args = len(sys.argv)
+    config_filename = str(sys.argv[1])
+    if n_args > 2:
+        desc = str(sys.argv[2])
+    else:
+        desc=''
 
-# open config file ---------------------------------
-with open(config_filename, 'r') as ymlfile:
-    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    # open config file ---------------------------------
+    with open(config_filename, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
-write_filename = create_filename()
+    write_filename = create_filename()
 
-# voltage bias things ------------------------------
-if cfg['voltage_bias']['source'].lower() in ['bluebox','blue box']:
-    voltage_source = 'bluebox'
-else:
-    voltage_source = None
-if cfg['voltage_bias']['overbias']:
-    to_normal_method='overbias'
-    overbias_temp_k=cfg['voltage_bias']['overbias_temp_k']
-else:
-    to_normal_method=None
-    overbias_temp_k=None
-bayname = handle_column(cfg['detectors']['Column'])
-dacs = np.linspace(int(cfg['voltage_bias']['v_start_dac']),int(cfg['voltage_bias']['v_stop_dac']),int(cfg['voltage_bias']['npts']))
+    # voltage bias things ------------------------------
+    if cfg['voltage_bias']['source'].lower() in ['bluebox','blue box']:
+        voltage_source = 'bluebox'
+    else:
+        voltage_source = None
+    if cfg['voltage_bias']['overbias']:
+        to_normal_method='overbias'
+        overbias_temp_k=cfg['voltage_bias']['overbias_temp_k']
+    else:
+        to_normal_method=None
+        overbias_temp_k=None
+    bayname = handle_column(cfg['detectors']['Column'])
+    dacs = np.linspace(int(cfg['voltage_bias']['v_start_dac']),int(cfg['voltage_bias']['v_stop_dac']),int(cfg['voltage_bias']['npts']))
 
-# bath temperature things ---------------------------
-bath_temps = cfg['runconfig']['bathTemperatures']
-# below commented out because it can crash adr_gui.  Need to ask what the set-temp is through adr_gui, and this is currently not in the software.
-# if bath_temps==[0]: # take at current temperature only
-#     from instruments import Lakeshore370
-#     ls370 = Lakeshore370() 
-#     bath_temps = [ls370.getTemperatureSetPoint()]
+    # bath temperature things ---------------------------
+    bath_temps = cfg['runconfig']['bathTemperatures']
+    # below commented out because it can crash adr_gui.  Need to ask what the set-temp is through adr_gui, and this is currently not in the software.
+    # if bath_temps==[0]: # take at current temperature only
+    #     from instruments import Lakeshore370
+    #     ls370 = Lakeshore370() 
+    #     bath_temps = [ls370.getTemperatureSetPoint()]
 
-#############
-pt_taker = IVPointTaker(db_cardname=cfg['voltage_bias']['db_cardname'], bayname=bayname, voltage_source = voltage_source)
-curve_taker = IVCurveTaker(pt_taker, temp_settle_delay_s=cfg['runconfig']['temp_settle_delay_s'], shock_normal_dac_value=cfg['voltage_bias']['shock_normal_dac_value'], 
-                           zero_tower_at_end=cfg['voltage_bias']['setVtoZeroPostIV'], adr_gui_control=None)
-curve_taker.prep_fb_settings(I=cfg['dfb']['i'], fba_offset=cfg['dfb']['dac_a_offset'], ARLoff=True)
-ivsweeper = IVTempSweeper(curve_taker, to_normal_method=to_normal_method, overbias_temp_k=overbias_temp_k, overbias_dac_value = cfg['voltage_bias']['v_start_dac'])
+    #############
+    pt_taker = iv.IVPointTaker(db_cardname=cfg['voltage_bias']['db_cardname'], bayname=bayname, voltage_source = voltage_source)
+    curve_taker = iv.IVCurveTaker(pt_taker, temp_settle_delay_s=cfg['runconfig']['temp_settle_delay_s'], shock_normal_dac_value=cfg['voltage_bias']['shock_normal_dac_value'], 
+                            zero_tower_at_end=cfg['voltage_bias']['setVtoZeroPostIV'], adr_gui_control=None)
+    curve_taker.prep_fb_settings(I=cfg['dfb']['i'], fba_offset=cfg['dfb']['dac_a_offset'], ARLoff=True)
+    ivsweeper = iv.IVTempSweeper(curve_taker, to_normal_method=to_normal_method, overbias_temp_k=overbias_temp_k, overbias_dac_value = cfg['voltage_bias']['v_start_dac'])
 
-if is_coldload_sweep():
-    cl_temps_k = cfg['coldload']['cl_temps_k']
-    clsweep_taker = IVColdloadSweeper(ivsweeper)
-    data = clsweep_taker.get_sweep(dacs, cl_temps_k, bath_temps,
-                                   cl_temp_tolerance_k=cfg['coldload']['cl_temp_tolerance_k'],
-                                   cl_settemp_timeout_m=10.0,
-                                   cl_post_setpoint_waittime_m=cfg['coldload']['cl_post_setpoint_waittime_m'],
-                                   skip_first_settle = cfg['coldload']['immediateFirstMeasurement'],
-                                   cool_upon_finish = cfg['coldload']['cool_upon_finish'], extra_info={'config': cfg, 'exp_status':desc},
-                                   write_while_acquire = True, filename=write_filename)
-else:
-    data = ivsweeper.get_sweep(dacs, bath_temps, extra_info={'config':cfg,'exp_status':desc,'row_to_period_map':cfg['detectors']['Rows']})
-    data.to_file(write_filename,overwrite=True)
-    if cfg['runconfig']['show_plot']:
-        print('showing plot')
-        plot_iv_groups(data)
+    if is_coldload_sweep():
+        cl_temps_k = cfg['coldload']['cl_temps_k']
+        clsweep_taker = iv.IVColdloadSweeper(ivsweeper)
+        data = clsweep_taker.get_sweep(dacs, cl_temps_k, bath_temps,
+                                    cl_temp_tolerance_k=cfg['coldload']['cl_temp_tolerance_k'],
+                                    cl_settemp_timeout_m=10.0,
+                                    cl_post_setpoint_waittime_m=cfg['coldload']['cl_post_setpoint_waittime_m'],
+                                    skip_first_settle = cfg['coldload']['immediateFirstMeasurement'],
+                                    cool_upon_finish = cfg['coldload']['cool_upon_finish'], extra_info={'config': cfg, 'exp_status':desc},
+                                    write_while_acquire = True, filename=write_filename)
+    else:
+        data = ivsweeper.get_sweep(dacs, bath_temps, extra_info={'config':cfg,'exp_status':desc,'row_to_period_map':cfg['detectors']['Rows']})
+        data.to_file(write_filename,overwrite=True)
+        if cfg['runconfig']['show_plot']:
+            print('showing plot')
+            plot_iv_groups(data)
 
-if cfg['runconfig']['save_data']:
-    data.to_file(write_filename,overwrite=True)
-    print('data aquistion finished.\nWrote to file:',write_filename)
-else:
-    is_save = input('You have selected not to save data.  If you would like to save the file, hit 1 and enter.')
-    if is_save=='1': 
+    if cfg['runconfig']['save_data']:
         data.to_file(write_filename,overwrite=True)
         print('data aquistion finished.\nWrote to file:',write_filename)
     else:
-        print('Exiting without saving')
+        is_save = input('You have selected not to save data.  If you would like to save the file, hit 1 and enter.')
+        if is_save=='1': 
+            data.to_file(write_filename,overwrite=True)
+            print('data aquistion finished.\nWrote to file:',write_filename)
+        else:
+            print('Exiting without saving')
+
+if __name__ == "__main__":
+    main()
