9822f9389f1e49270a004e8974b27dbab373a54b
Christopher Rooney <christopher.rooney@nist.gov>
2024-10-21 16:06:31 -0600

    Enable waiting just a bit longer for ADR to cool before giving up

--- adr_gui/adr_gui.py
+++ adr_gui/adr_gui.py
@@ -676,7 +676,7 @@ class ADR_Gui(PyQt5.QtWidgets.QMainWindow):
         t = time.localtime()
         h = numpy.floor(startTime24Hour)
         m = numpy.floor((startTime24Hour-h)*60)
-        if t.tm_hour == h and t.tm_min==m and self.enableTimeBasedMagCheckbox.isChecked():
+        if t.tm_hour == h and m<=t.tm_min<=m+2 and self.enableTimeBasedMagCheckbox.isChecked():
             if self.lastTemp_K > 4:
                 print(("not magging up because temp is %f, not below 4 K"%self.lastTemp_K))
                 self.ensureHeatSwitchIsClosed()
